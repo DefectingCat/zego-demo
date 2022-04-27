c6c65e25383f9f23a7f26d6d8d513bab962ba0f7
Defectink <i@defect.ink>
2022-04-27 10:21:37 +0800

    Add base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('pages/Home'));
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback>
+              <Home />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <div className="p-8 flex">
+        <div className="p-4 bg-white shadow-lg rounded-md">
+          <Link to="/server">
+            <h1 className="text-xl">Server</h1>
+          </Link>
+        </div>
+
+        <div className="p-4 bg-white shadow-lg rounded-md">
+          <Link to="/client">
+            <h1 className="text-xl">Client</h1>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
