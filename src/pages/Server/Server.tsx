b5ac23fd83d957d13691cfb99445d00b900d89fb
Defectink <i@defect.ink>
2022-04-27 16:43:27 +0800

    Add video

--- src/pages/Server/Server.tsx
+++ src/pages/Server/Server.tsx
@@ -1,9 +1,15 @@
 import { ZegoExpressEngine } from 'zego-express-engine-webrtc';
 import cn from 'classnames';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { ReactComponent as MicroPhone } from 'assets/video/microphone.svg';
 import { ReactComponent as Camera } from 'assets/video/camera.svg';
 import { Button, Popover } from '@nextui-org/react';
+import { useImmer } from 'use-immer';
+import {
+  ZegoDeviceInfo,
+  ZegoLocalStreamConfig,
+  ZegoWebPlayOption,
+} from 'zego-express-engine-webrtc/sdk/code/zh/ZegoExpressEntity.web';
 
 const appID = 1237665297;
 const server = 'wss://webliveroom1237665297-api.imzego.com/ws';
@@ -16,8 +22,41 @@ const deviceValid: {
 };
 
 const Server = () => {
+  // 房间信息
+  const [roomState, setRoomState] = useImmer({
+    roomId: '',
+    userId: '',
+    userName: '',
+    token: '',
+  });
+  // 模拟销售人员
+  useEffect(() => {
+    setRoomState((d) => {
+      d.userId = 'xfy';
+      d.roomId = '1';
+      d.userName = 'xfy';
+      d.token =
+        '03AAAAAGJqMZcAEGQwbnBzcnVpNXMwZjJhaDQAoNQlcGaA6GvqwLQgXcXgAknbw1OF6TulvinIFtBPwwy8O7wGnY+pgtwJV8klk/kEpisQzwmebfjQ51jWzSp83h2NEpNMyDkOFP4tset8aO2qXHSZqrZ8lBFOZ9fiXAi2/Ggrp0kzZJpllX5v65/TaQYUmvX5UX+JC3IzXGBAJz+dFCLxg5pHLaIaQ/dEbDGvoiP5zjVw1k+4oOstL18W9MY=';
+
+      // loginRoom(d.roomId, d.userId,d.userName, d.token);
+    });
+  }, [setRoomState]);
+  // 登录到房间
+  useEffect(() => {
+    if (roomState.roomId !== '')
+      loginRoom(
+        roomState.roomId,
+        roomState.userId,
+        roomState.userName,
+        roomState.token
+      );
+  }, [roomState]);
+
+  // Step 1: Create engine
+  const zg = useRef(new ZegoExpressEngine(appID, server));
+
   // 连接状态
-  const [connectStatus, setConnectStatus] = useState('');
+  const [connectStatus, setConnectStatus] = useState('DISCONNECTED');
   const connectStatusValid = useMemo(
     () => ({
       DISCONNECTED: setConnectStatus('DISCONNECTED'),
@@ -26,9 +65,8 @@ const Server = () => {
     }),
     []
   );
-
   // 流 ID
-  const [publishInfoStreamID, setPublishInfoStreamID] = useState('');
+  const [publishInfoStreamID, setPublishInfoStreamID] = useState('0001');
   const publishInfoStreamIDValid = useMemo(
     () => ({
       PUBLISHING: (streamID: string) => setPublishInfoStreamID(streamID),
@@ -41,6 +79,19 @@ const Server = () => {
     []
   );
 
+  // Init engine event
+  zg.current.on(
+    'roomStateUpdate',
+    (roomID, state) => connectStatusValid[state]
+  );
+  zg.current.on('publisherStateUpdate', ({ state, streamID }) =>
+    publishInfoStreamIDValid[state](streamID)
+  );
+  zg.current.on('playerStateUpdate', ({ state, streamID }) =>
+    publishInfoStreamIDValid[state](streamID)
+  );
+
+  // Step 2: Check system requirements
   // 设备权限状态
   const [deviceStatus, setDeviceStatus] = useState<{
     [key: string]: boolean;
@@ -50,24 +101,10 @@ const Server = () => {
     camera: false,
     microphone: false,
   });
-
-  // Step 1: Create engine
-  const zg = new ZegoExpressEngine(appID, server);
-
-  // Init engine event
-  zg.on('roomStateUpdate', (roomID, state) => connectStatusValid[state]);
-  zg.on('publisherStateUpdate', ({ state, streamID }) =>
-    publishInfoStreamIDValid[state](streamID)
-  );
-  zg.on('playerStateUpdate', ({ state, streamID }) =>
-    publishInfoStreamIDValid[state](streamID)
-  );
-
-  // Step 2: Check system requirements
   async function checkSystemRequirements() {
-    console.log('sdk version is', zg.getVersion());
+    console.log('sdk version is', zg.current.getVersion());
     try {
-      const result = await zg.checkSystemRequirements();
+      const result = await zg.current.checkSystemRequirements();
 
       console.warn('checkSystemRequirements ', result);
 
@@ -94,6 +131,7 @@ const Server = () => {
         camera: true,
         microphone: true,
       });
+      await enumerateDevices();
       return true;
     } catch (err) {
       console.error('checkSystemRequirements', err);
@@ -101,11 +139,140 @@ const Server = () => {
     }
   }
 
+  // 设备列表
+  const [device, setDevice] = useImmer<{
+    audioDeviceList: ZegoDeviceInfo[];
+    videoDeviceList: ZegoDeviceInfo[];
+    microphoneDevicesVal: string;
+    cameraDevicesVal: string;
+  }>({
+    audioDeviceList: [],
+    videoDeviceList: [],
+    microphoneDevicesVal: '',
+    cameraDevicesVal: '',
+  });
+  // 枚举设备
+  async function enumerateDevices() {
+    const deviceInfo = await zg.current.enumDevices();
+    const audioDeviceList =
+      deviceInfo &&
+      deviceInfo.microphones.map((item, index) => {
+        if (!item.deviceName) {
+          item.deviceName = 'microphone' + index;
+        }
+        console.log('microphone: ' + item.deviceName);
+        return item;
+      });
+    audioDeviceList.push({ deviceID: '0', deviceName: '禁止' });
+    const videoDeviceList =
+      deviceInfo &&
+      deviceInfo.cameras.map((item, index) => {
+        if (!item.deviceName) {
+          item.deviceName = 'camera' + index;
+        }
+        console.log('camera: ' + item.deviceName);
+        return item;
+      });
+    videoDeviceList.push({ deviceID: '0', deviceName: '禁止' });
+    setDevice((d) => {
+      d.audioDeviceList = audioDeviceList;
+      d.videoDeviceList = videoDeviceList;
+      d.microphoneDevicesVal = audioDeviceList[0].deviceID;
+      d.cameraDevicesVal = videoDeviceList[0].deviceID;
+    });
+  }
+
+  // 是否登录
+  const [isLogin, setIsLogin] = useState(false);
+  // Step 3: login to room
+  async function loginRoom(
+    roomId: string,
+    userId: string,
+    userName: string,
+    token: string
+  ) {
+    try {
+      await zg.current.loginRoom(roomId, token, {
+        userID: userId,
+        userName,
+      });
+      setIsLogin(true);
+    } catch (e) {
+      setIsLogin(false);
+      console.error(e);
+    }
+  }
+
+  // Step 4: publish stream
+  // 流编码
+  const videoCodec =
+    localStorage.getItem('VideoCodec') === 'H.264' ? 'H264' : 'VP8';
+
+  // 推流信息
+  const localStream = useRef<MediaStream | null>(null);
+  // 本机摄像头 video ref
+  const publishVideoRef = useRef<HTMLVideoElement>(null);
+
+  // 流
+  // Step 5: play stream
+  // 拉流信息
+  const remoteStream = useRef<MediaStream | null>(null);
+  // 远端摄像头 video ref
+  const playVideoRef = useRef<HTMLVideoElement>(null);
+  async function playStream(streamID: string, options: ZegoWebPlayOption = {}) {
+    try {
+      const stream = await zg.current.startPlayingStream(streamID, options);
+      remoteStream.current = stream;
+      if (!playVideoRef.current) throw new Error('playVideoRef is null');
+      playVideoRef.current.srcObject = remoteStream.current;
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
   // Handlers
+  const [systemRequireStatus, setSystemRequireStatus] = useState(false);
   const handleVideo = async () => {
-    const result = await checkSystemRequirements();
-    console.log(result);
+    setSystemRequireStatus(await checkSystemRequirements());
   };
+  useEffect(() => {
+    systemRequireStatus &&
+      publishStream(publishInfoStreamID, {
+        camera: {
+          audioInput: device.microphoneDevicesVal,
+          videoInput: device.cameraDevicesVal,
+          video: deviceStatus.camera,
+          audio: deviceStatus.microphone,
+        },
+      });
+
+    async function publishStream(
+      streamID: string,
+      config: ZegoLocalStreamConfig
+    ) {
+      try {
+        const stream = await zg.current.createStream(config);
+        localStream.current = stream;
+
+        zg.current.startPublishingStream(
+          streamID,
+          localStream.current ?? stream,
+          {
+            videoCodec,
+          }
+        );
+
+        if (!publishVideoRef.current)
+          throw new Error('publishVideoRef is null');
+        publishVideoRef.current.srcObject = localStream.current;
+
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  }, [systemRequireStatus]);
 
   return (
     <>
@@ -118,7 +285,17 @@ const Server = () => {
       >
         {/* 标头 */}
         <div className="py-4 flex justify-between">
-          <h1 className="text-2xl font-medium">云汇展在线工作人员</h1>
+          <div className="flex items-center">
+            <span className="text-2xl font-medium mr-2">
+              云汇展在线工作人员
+            </span>
+            <span
+              className={cn(
+                'w-4 h-4 rounded-full',
+                isLogin ? 'bg-green-600' : 'bg-red-600'
+              )}
+            ></span>
+          </div>
           <div
             className={cn(
               'rounded bg-red-500 font-bold w-[32px]',
@@ -145,7 +322,7 @@ const Server = () => {
             <Popover.Content>
               <div className="p-4">
                 {Object.keys(deviceStatus).map((k) => (
-                  <div id={k} className="flex">
+                  <div key={k} className="flex">
                     {deviceValid[k]}：
                     {deviceStatus[k] ? (
                       <div className="flex items-center">
@@ -178,6 +355,14 @@ const Server = () => {
         <div className="flex justify-end">
           <Button>发送</Button>
         </div>
+      </div>
+
+      <div>
+        <video ref={publishVideoRef} autoPlay></video>
+      </div>
+
+      <div>
+        <video ref={playVideoRef} autoPlay></video>
       </div>
     </>
   );
